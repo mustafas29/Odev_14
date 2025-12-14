872b3b9f33e106b3fca0223e6a8ee84110e6d05c
mustafas29 <mustafasengul29@gmail.com>
2025-12-14 23:35:13 +0300

    Update AddRecordModal.java

--- src/main/java/pages/AddRecordModal.java
+++ src/main/java/pages/AddRecordModal.java
@@ -1,16 +1,31 @@
 package pages;
 
- /**
- * -----------------------------------------------------------------------------
- * CLASS NAME   : AddRecordModal
- * AUTHOR       : Mustafa.Sengul
- * CREATED DATE : 12.12.2025
- * DESCRIPTION  : [Kısa açıklama yazılabilir]
- * -----------------------------------------------------------------------------
- * UPDATE LOG   : 
- *   - [TARİH] : [KULLANICI ADI] - [Yapılan değişiklik açıklaması]
- * -----------------------------------------------------------------------------
- */
- 
-public class AddRecordModal {
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+
+public class AddRecordModal extends BasePage {
+
+    private final SelenideElement firstName = $("#firstName");
+    private final SelenideElement lastName = $("#lastName");
+    private final SelenideElement email = $("#userEmail");
+    private final SelenideElement age = $("#age");
+    private final SelenideElement salary = $("#salary");
+    private final SelenideElement department = $("#department");
+    private final SelenideElement submit = $("#submit");
+
+    public void fill(String f, String l, String e, String a, String s, String d) {
+        firstName.setValue(f);
+        lastName.setValue(l);
+        email.setValue(e);
+        age.setValue(a);
+        salary.setValue(s);
+        department.setValue(d);
+        submit.click();
+    }
+
+    public void updateDepartment(String newDept) {
+        department.clear();
+        department.setValue(newDept);
+        submit.click();
+    }
 }
