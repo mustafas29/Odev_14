ccbcdeea5a2bc62226a90195c38409904e90ff5d
mustafas29 <mustafasengul29@gmail.com>
2025-12-14 23:36:16 +0300

    Implement Buttons menu interaction in ElementsPage
    Added functionality to click the Buttons menu in ElementsPage.

--- src/main/java/pages/ElementsPage.java
+++ src/main/java/pages/ElementsPage.java
@@ -1,16 +1,15 @@
 package pages;
 
- /**
- * -----------------------------------------------------------------------------
- * CLASS NAME   : ElementsPage
- * AUTHOR       : Mustafa.Sengul
- * CREATED DATE : 12.12.2025
- * DESCRIPTION  : [Kısa açıklama yazılabilir]
- * -----------------------------------------------------------------------------
- * UPDATE LOG   : 
- *   - [TARİH] : [KULLANICI ADI] - [Yapılan değişiklik açıklaması]
- * -----------------------------------------------------------------------------
- */
- 
-public class ElementsPage {
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Condition.visible;
+
+public class ElementsPage extends BasePage {
+    // More specific selector for the Buttons menu
+    private final SelenideElement buttonsMenu = $("div.element-list.collapse.show ul.menu-list li#item-4");
+
+    public void clickButtonsMenu() {
+        System.out.println("Clicking on Buttons menu...");
+        buttonsMenu.shouldBe(visible).click();
+    }
 }
