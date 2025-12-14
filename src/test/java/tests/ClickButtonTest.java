70533d70369e231e46ad598dd80bf5162c0a9268
mustafas29 <mustafasengul29@gmail.com>
2025-12-14 23:38:17 +0300

    Implement button click test in ClickButtonTest
    Added a test case to validate button click functionality on the demo page.

--- src/test/java/tests/ClickButtonTest.java
+++ src/test/java/tests/ClickButtonTest.java
@@ -1,16 +1,34 @@
 package tests;
 
- /**
- * -----------------------------------------------------------------------------
- * CLASS NAME   : ClickButtonTest
- * AUTHOR       : Mustafa.Sengul
- * CREATED DATE : 12.12.2025
- * DESCRIPTION  : [Kısa açıklama yazılabilir]
- * -----------------------------------------------------------------------------
- * UPDATE LOG   : 
- *   - [TARİH] : [KULLANICI ADI] - [Yapılan değişiklik açıklaması]
- * -----------------------------------------------------------------------------
- */
- 
-public class ClickButtonTest {
+import base.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.ButtonsPage;
+import pages.ElementsPage;
+import static com.codeborne.selenide.Selenide.*;
+
+public class ClickButtonTest extends TestBase {
+
+    @Test
+    public void clickButtonTest() {
+        // Sayfayı aç
+        open("https://demoqa.com/elements");
+
+        // Sayfa nesnelerini oluştur
+        ElementsPage elements = new ElementsPage();
+        ButtonsPage buttons = new ButtonsPage();
+
+        // Butonlar menüsüne tıkla
+        elements.clickButtonsMenu();
+
+        // 'Click Me' butonuna tıkla
+        buttons.clickClickMe();
+
+        // Mesajı doğrula
+        String actualMessage = buttons.getMessage();
+        String expectedMessage = "You have done a dynamic click";
+
+        Assert.assertEquals(actualMessage, expectedMessage,
+                String.format("Beklenen mesaj: '%s', ancak alınan: '%s'", expectedMessage, actualMessage));
+    }
 }
