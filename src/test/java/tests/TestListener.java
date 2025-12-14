5629e223a7459a2a8550ebfd59c74e261b91db3b
mustafas29 <mustafasengul29@gmail.com>
2025-12-14 23:38:44 +0300

    Add TestListener for logging test events
    Implemented TestListener to log test execution events including start, success, failure, and skip. Added screenshot functionality on test failure.

--- src/test/java/tests/TestListener.java
+++ src/test/java/tests/TestListener.java
@@ -1,16 +1,48 @@
 package tests;
 
- /**
- * -----------------------------------------------------------------------------
- * CLASS NAME   : TestListener
- * AUTHOR       : Mustafa.Sengul
- * CREATED DATE : 12.12.2025
- * DESCRIPTION  : [Kısa açıklama yazılabilir]
- * -----------------------------------------------------------------------------
- * UPDATE LOG   : 
- *   - [TARİH] : [KULLANICI ADI] - [Yapılan değişiklik açıklaması]
- * -----------------------------------------------------------------------------
- */
- 
-public class TestListener {
+import com.codeborne.selenide.Selenide;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("TEST STARTED --> " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("TEST PASSED --> " + result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("TEST FAILED --> " + result.getName());
+
+        // Screenshot
+        String screenshot = Selenide.screenshot("FAILED_" + result.getName());
+        System.out.println("Screenshot saved: " + screenshot);
+    }
+    
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("TEST SKIPPED --> " + result.getName());
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        // Bu metod genellikle boş bırakılır
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("=== TEST EXECUTION STARTED ===");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("=== ALL TESTS FINISHED ===");
+    }
 }
