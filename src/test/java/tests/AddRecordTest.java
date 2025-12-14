1130f835f0fd99e992fc65e425cae79c3ae5cfad
mustafas29 <mustafasengul29@gmail.com>
2025-12-14 23:37:53 +0300

    Update AddRecordTest.java

--- src/test/java/tests/AddRecordTest.java
+++ src/test/java/tests/AddRecordTest.java
@@ -1,16 +1,28 @@
 package tests;
 
- /**
- * -----------------------------------------------------------------------------
- * CLASS NAME   : AddRecordTest
- * AUTHOR       : Mustafa.Sengul
- * CREATED DATE : 12.12.2025
- * DESCRIPTION  : [Kısa açıklama yazılabilir]
- * -----------------------------------------------------------------------------
- * UPDATE LOG   : 
- *   - [TARİH] : [KULLANICI ADI] - [Yapılan değişiklik açıklaması]
- * -----------------------------------------------------------------------------
- */
- 
-public class AddRecordTest {
+import base.TestBase;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.AddRecordModal;
+import pages.WebTablesPage;
+
+public class AddRecordTest extends TestBase {
+
+    @Test
+    public void addAndEditRecordTest() {
+        WebTablesPage table = new WebTablesPage();
+        AddRecordModal modal = new AddRecordModal();
+
+        table.openUrl("https://demoqa.com/webtables");
+
+        table.clickAdd();
+        modal.fill("Mustafa", "Sengul", "test@test.com", "35", "5000", "QA");
+
+        Assert.assertTrue(table.getFirstCellText().contains("Mustafa"));
+
+        table.clickEdit();
+        modal.updateDepartment("Automation");
+
+        Assert.assertTrue(table.getFirstCellText().contains("Automation"));
+    }
 }
